43dd3ad505a1a1c3c1d57d0bcc5e655bd03dca50
TomasTvrdy <tvrdytom@gmail.com>
2016-11-16 02:39:08 +0100

    Compare local and ns version

--- extension.js
+++ extension.js
@@ -25,6 +25,32 @@ function downloadFileFromNetSuite(fullFileName) {
     });
 }
 
+function previewFileFromNetSuite(file) {
+    var relativeFileName = getRelativePath(file.fsPath);
+    
+    var client = new RestClient();
+    var args = {
+        path: { fileName: relativeFileName },
+        headers: {                
+            "Content-Type": "text/plain",
+            "Authorization": vscode.workspace.getConfiguration('netSuiteUpload')['authentication']
+        }
+    };
+
+    var baseRestletURL = vscode.workspace.getConfiguration('netSuiteUpload')['restlet'];
+    client.get(baseRestletURL + '&fileName=${fileName}', args, function (data) {
+
+        var tempFolder = vscode.workspace.getConfiguration('netSuiteUpload')['tempFolder'];
+        var filePathArray = (relativeFileName.split('.')[0] + '.preview.' + relativeFileName.split('.')[1]).split('\\');
+        var newPreviewFile = tempFolder + '\\' + filePathArray[filePathArray.length-1];
+
+        fs.writeFile(newPreviewFile, data.toString());
+
+        var nsFile = vscode.Uri.file(newPreviewFile);
+        vscode.commands.executeCommand('vscode.diff', file, nsFile, 'Local <--> NetSuite');        
+    });
+}
+
 function activate(context) {
     console.log('Extension "netsuite-upload" is now active!');
 
@@ -39,12 +65,32 @@ function activate(context) {
     });
     context.subscriptions.push(downloadFileDisposable);
 
+    let previewFileDisposable = vscode.commands.registerCommand('extension.previewFile', (file) => {
+        // Root SuiteScript folder has to be opened 
+        if (!vscode.workspace.rootPath) {
+            vscode.window.showErrorMessage('No project is opened. Please open root folder. (SuiteScripts)');
+            return;
+        }
+        
+        previewFileFromNetSuite(file);
+    });
+    context.subscriptions.push(previewFileDisposable);
+
+
+
     let uploadFileDisposable = vscode.commands.registerCommand('extension.uploadFile', (file) => {
 
         // TODO:
         
     });
     context.subscriptions.push(uploadFileDisposable);
+
+    let downloadFolderDisposable = vscode.commands.registerCommand('extension.downloadFolder', (folder) => {
+
+        // TODO:
+        
+    });
+    context.subscriptions.push(downloadFolderDisposable);
 
     
 }
