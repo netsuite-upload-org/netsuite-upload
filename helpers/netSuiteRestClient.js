a409a6a90e5f4568a82b76a36bbb18b1c66f535f
jonlamb <jonnylamb@gmail.com>
2017-08-06 16:51:31 -0500

    Added Oath feature

--- helpers/netSuiteRestClient.js
+++ helpers/netSuiteRestClient.js
@@ -1,5 +1,7 @@
 let vscode = require('vscode');
 let RestClient = require('node-rest-client').Client;
+let OAuth = require('oauth-1.0a');
+let crypto  = require('crypto');
 
 function getRelativePath(absFilePath) {
     return absFilePath.slice(vscode.workspace.rootPath.length);
@@ -26,6 +28,29 @@ function getData(type, objectPath, callback) {
     };
 
     var baseRestletURL = vscode.workspace.getConfiguration('netSuiteUpload')['restlet'];
+
+    // Support for Oath authentication
+    if (!args.headers.Authorization) {    
+        var oauth = OAuth({
+            consumer: {
+                key: vscode.workspace.getConfiguration('netSuiteUpload')['netsuite-key'],
+                secret: vscode.workspace.getConfiguration('netSuiteUpload')['netsuite-secret']
+            },
+            signature_method: 'HMAC-SHA1',
+            hash_function: function(base_string, key) {
+                return crypto.createHmac('sha1', key).update(base_string).digest('base64');
+            }
+        });
+        var token = {
+            key: vscode.workspace.getConfiguration('netSuiteUpload')['consumer-token'],
+            secret: vscode.workspace.getConfiguration('netSuiteUpload')['consumer-secret']
+        };
+        var headerWithRealm = oauth.toHeader(oauth.authorize({ url: baseRestletURL, method: 'POST' }, token));
+        headerWithRealm.Authorization += ', realm="' + vscode.workspace.getConfiguration('netSuiteUpload')['realm'] + '"';
+        headerWithRealm['Content-Type'] = 'application/json';
+        args.headers = headerWithRealm;
+    }
+
     client.get(baseRestletURL + '&type=' + type + '&name=${name}', args, function (data) {
         callback(data);
     });
@@ -50,8 +75,30 @@ function postData(type, objectPath, content, callback) {
             content: content
         }
     };
-
+    
     var baseRestletURL = vscode.workspace.getConfiguration('netSuiteUpload')['restlet'];
+
+    // Support for Oath authentication
+    if (!args.headers.Authorization) {    
+        var oauth = OAuth({
+            consumer: {
+                key: vscode.workspace.getConfiguration('netSuiteUpload')['netsuite-key'],
+                secret: vscode.workspace.getConfiguration('netSuiteUpload')['netsuite-secret']
+            },
+            signature_method: 'HMAC-SHA1',
+            hash_function: function(base_string, key) {
+                return crypto.createHmac('sha1', key).update(base_string).digest('base64');
+            }
+        });
+        var token = {
+            key: vscode.workspace.getConfiguration('netSuiteUpload')['consumer-token'],
+            secret: vscode.workspace.getConfiguration('netSuiteUpload')['consumer-secret']
+        };
+        var headerWithRealm = oauth.toHeader(oauth.authorize({ url: baseRestletURL, method: 'POST' }, token));
+        headerWithRealm.Authorization += ', realm="' + vscode.workspace.getConfiguration('netSuiteUpload')['realm'] + '"';
+        headerWithRealm['Content-Type'] = 'application/json';
+        args.headers = headerWithRealm;
+    }
     client.post(baseRestletURL, args, function (data) {
         callback(data);
     });
@@ -74,6 +121,29 @@ function deletetData(type, objectPath, callback) {
     };
 
     var baseRestletURL = vscode.workspace.getConfiguration('netSuiteUpload')['restlet'];
+
+    // Support for Oath authentication
+    if (!args.headers.Authorization) {    
+        var oauth = OAuth({
+            consumer: {
+                key: vscode.workspace.getConfiguration('netSuiteUpload')['netsuite-key'],
+                secret: vscode.workspace.getConfiguration('netSuiteUpload')['netsuite-secret']
+            },
+            signature_method: 'HMAC-SHA1',
+            hash_function: function(base_string, key) {
+                return crypto.createHmac('sha1', key).update(base_string).digest('base64');
+            }
+        });
+        var token = {
+            key: vscode.workspace.getConfiguration('netSuiteUpload')['consumer-token'],
+            secret: vscode.workspace.getConfiguration('netSuiteUpload')['consumer-secret']
+        };
+        var headerWithRealm = oauth.toHeader(oauth.authorize({ url: baseRestletURL, method: 'POST' }, token));
+        headerWithRealm.Authorization += ', realm="' + vscode.workspace.getConfiguration('netSuiteUpload')['realm'] + '"';
+        headerWithRealm['Content-Type'] = 'application/json';
+        args.headers = headerWithRealm;
+    }
+
     client.delete(baseRestletURL + '&type=' + type + '&name=${name}', args, function (data) {
         callback(data);
     });
