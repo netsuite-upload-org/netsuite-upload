73cc0d295efd6f357a525c176454f2f59c87634f
Larry Silverman <lsilverman@trackabout.com>
2019-01-23 16:57:44 -0600

    The updated version of node-rest-client broke some things. Fixing. Still not certain OAuth is working right.

--- helpers/netSuiteRestClient.js
+++ helpers/netSuiteRestClient.js
@@ -3,7 +3,8 @@
 let vscode = require('vscode');
 let RestClient = require('node-rest-client').Client;
 let OAuth = require('oauth-1.0a');
-let crypto  = require('crypto');
+let crypto = require('crypto');
+let url = require('url');
 
 function getRelativePath(absFilePath) {
     return absFilePath.slice(vscode.workspace.rootPath.length);
@@ -18,24 +19,21 @@ function getDirectory(directory, callback) {
 }
 
 function getHttpHeaders() {
-
     var oldAuth = vscode.workspace.getConfiguration('netSuiteUpload').authentication;
     var newAuth = vscode.workspace.getConfiguration('netSuiteUpload').netSuiteKey;
-    if (oldAuth && oldAuth.length > 0)
-    {
+    if (oldAuth && oldAuth.length > 0) {
         return {
-            "Authorization" : vscode.workspace.getConfiguration('netSuiteUpload').authentication,
-            "Content-Type" : "application/json"
+            "Authorization": vscode.workspace.getConfiguration('netSuiteUpload').authentication,
+            "Content-Type": "application/json"
         };
-    }
-    else if (newAuth && newAuth.length > 0 ) {
+    } else if (newAuth && newAuth.length > 0) {
         var oauth = OAuth({
             consumer: {
                 key: vscode.workspace.getConfiguration('netSuiteUpload').consumerToken,
                 secret: vscode.workspace.getConfiguration('netSuiteUpload').consumerSecret
             },
             signature_method: 'HMAC-SHA256',
-            hash_function: function(base_string, key) {
+            hash_function: function (base_string, key) {
                 return crypto.createHmac('sha256', key).update(base_string).digest('base64');
             }
         });
@@ -44,7 +42,10 @@ function getHttpHeaders() {
             secret: vscode.workspace.getConfiguration('netSuiteUpload').netSuiteSecret
         };
         var baseRestletURL = vscode.workspace.getConfiguration('netSuiteUpload').restlet;
-        var headerWithRealm  = oauth.toHeader(oauth.authorize({ url: baseRestletURL, method: 'POST' }, token));
+        var headerWithRealm = oauth.toHeader(oauth.authorize({
+            url: baseRestletURL,
+            method: 'POST'
+        }, token));
         headerWithRealm.Authorization += ', realm="' + vscode.workspace.getConfiguration('netSuiteUpload').realm + '"';
         headerWithRealm["Content-Type"] = "application/json";
         return headerWithRealm;
@@ -55,15 +56,30 @@ function getHttpHeaders() {
 function getData(type, objectPath, callback) {
     var relativeName = getRelativePath(objectPath);
 
+    // node-rest-client doesn't want querystring params to be already in the URL if using the args.parameters.
+    // So we need to strip them off and add them to args.
+    var parsed_url = url.parse(vscode.workspace.getConfiguration('netSuiteUpload').restlet, true);
+
     var client = new RestClient();
     var args = {
-        path: { name: relativeName },
+        parameters: {
+            "type": type,
+            "name": relativeName,
+            "script": parsed_url.query.script,
+            "deploy": parsed_url.query.deploy
+        },
         headers: getHttpHeaders()
     };
 
-    var baseRestletURL = vscode.workspace.getConfiguration('netSuiteUpload').restlet;
+    // remove the url search and query components so we can re-form the URL without them.
+    parsed_url.search = parsed_url.query = "";
 
-    client.get(baseRestletURL + '&type=' + type + '&name=${name}', args, function (data) {
+    // reassemble the url
+    var baseRestletUrl = url.format(parsed_url);
+
+    client.get(baseRestletUrl, args, function (data, response) {
+        console.log(data);
+        console.log(response);
         callback(data);
     });
 }
@@ -79,33 +95,40 @@ function postData(type, objectPath, content, callback) {
     var args = {
         headers: getHttpHeaders(),
         data: {
-            type: 'file',
+            type: type,
             name: relativeName,
             content: content
         }
     };
 
     var baseRestletURL = vscode.workspace.getConfiguration('netSuiteUpload').restlet;
-    client.post(baseRestletURL, args, function (data) {
+    client.post(baseRestletURL, args, function (data, response) {
+        console.log(data);
+        console.log(response);
         callback(data);
     });
 }
 
 function deleteFile(file, callback) {
-    deletetData('file', file.fsPath, callback);
+    deleteData('file', file.fsPath, callback);
 }
 
-function deletetData(type, objectPath, callback) {
+function deleteData(type, objectPath, callback) {
     var relativeName = getRelativePath(objectPath);
 
     var client = new RestClient();
     var args = {
-        path: { name: relativeName },
+        path: {
+            "type": type,
+            "name": relativeName
+        },
         headers: getHttpHeaders()
     };
 
     var baseRestletURL = vscode.workspace.getConfiguration('netSuiteUpload').restlet;
-    client.delete(baseRestletURL + '&type=' + type + '&name=${name}', args, function (data) {
+    client.delete(baseRestletURL + '&type=${type}&name=${name}', args, function (data, response) {
+        console.log(data);
+        console.log(response);
         callback(data);
     });
 }
